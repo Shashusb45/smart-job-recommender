2af58dc4fac7018fda3bda9f5ef2c280cf1a3065
Shashusb45 <shashwathsbyakod45@gmail.com>
2025-11-12 23:55:45 +0530

    chore: init repo

---
+++ src/main/java/com/shashwath/smartjobrecommender/SmartJobRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.shashwath.smartjobrecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartJobRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartJobRecommenderApplication.class, args);
+	}
+
+}
